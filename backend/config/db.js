324e360ae0cc1cc16442b1a9b8d95234248e00d7
tenok94 <tenok94@gmail.com>
2024-12-15 15:55:50 -0300

    agregando mejoras en el mensaje

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,26 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
+
+// const dbconeted = async () => {
+//     try {
+//         await mongoose.connect("mongodb://localhost:27017/dbGestorDeTarea");
+//         console.log("Conectado a MongoDB");
+//     } catch (err){
+//         console.error("Error en la conexion a la base de datos ", err);
+//         process.exit(1); //detectamoes el proceso si hay un error grave en la conexión
+//     }
+// }
+
+// module.exports = dbconeted;
+const mongoose = require("mongoose");
 
 const dbconeted = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/dbGestorDeTarea");
+        const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/dbGestorDeTarea";
+        await mongoose.connect(MONGO_URI);
         console.log("Conectado a MongoDB");
-    } catch (err){
-        console.error("Error en la conexion a la base de datos ", err);
-        process.exit(1); //detectamoes el proceso si hay un error grave en la conexión
+    } catch (err) {
+        console.error("Error en la conexión a la base de datos ", err);
+        process.exit(1); // Detenemos el proceso si hay un error grave
     }
 }
 
